3f82ca0ab0aa72d7b92105442d1c648ba3d83254
Alfian <59092781+XpLeeCyTe666@users.noreply.github.com>
2020-10-05 13:14:07 +0700

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87541928fb3cc32d3d69';
+const key = '05c4f16d5341d572ad54ba2041c70243';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
     // Here you can change your position
     // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-    let latitude = 37.774929;
-    let longitude = -122.419418;
+    let latitude = -7.469437;
+    let longitude = 112.396986;
 
     getWeather(latitude, longitude);
 }
